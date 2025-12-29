e1a1aa25ffb75c54dd3183e18afebacff1b4fb92
Ashfi <ashfimohamed@gmail.com>
2025-12-29 21:17:21 +0500

    simple app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [task, setTask] = useState("")
+  const [taskList, setTaskList] = useState([])
+  const insertTask = (e) => {
+    e.preventDefault()
+    setTaskList((oldtaskList) => [...oldtaskList, task] )
+    setTask("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={insertTask} className='app'>
+          <h1>To Do List App</h1>
+          <label htmlFor='task'>Task
+            <input type='text' id='task' placeholder='add task' value={task} onChange={(e) => setTask(e.target.value)}></input>
+          </label>
+          <button >Add Task</button>
+        </form>
+        {taskList.length > 0 && (
+          <ul>
+            {taskList.map((item, id) => {
+             return( <li id={id}>{item}</li>)
+            })}
+          </ul>
+        ) }
     </div>
   );
 }
